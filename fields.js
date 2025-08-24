214f0a8eef0486936f7e28f49c5b1fdc66d9a022
Duster <ksduster@yahoo.com>
2025-08-24 12:35:01 -0500

    Create fields.js

---
+++ fields.js
@@ -0,0 +1,56 @@
+[
+  {
+    "name": "locomotiveUrl",
+    "label": "Locomotive Image URL",
+    "type": "text",
+    "value": ""
+  },
+  {
+    "name": "carUrl",
+    "label": "Car Image URL",
+    "type": "text",
+    "value": ""
+  },
+  {
+    "name": "smokeUrl",
+    "label": "Smoke Image URL",
+    "type": "text",
+    "value": ""
+  },
+  {
+    "name": "minDonations",
+    "label": "Minimum Donations To Start Train",
+    "type": "number",
+    "value": ""
+  },
+  {
+    "name": "cooldownMinutes",
+    "label": "Cooldown (Minutes)",
+    "type": "number",
+    "value": ""
+  },
+  {
+    "name": "trainDurationMinutes",
+    "label": "Train Duration (Minutes)",
+    "type": "number",
+    "value": ""
+  },
+  {
+    "name": "avatarSize",
+    "label": "Avatar Size (px)",
+    "type": "number",
+    "value": ""
+  },
+  {
+    "name": "smokeBaseAmount",
+    "label": "Smoke Base Amount (per stage)",
+    "type": "number",
+    "value": ""
+  },
+  {
+    "name": "smokeDonationThreshold",
+    "label": "Smoke Threshold ($ or Donations per stage)",
+    "type": "number",
+    "value": ""
+  }
+]
